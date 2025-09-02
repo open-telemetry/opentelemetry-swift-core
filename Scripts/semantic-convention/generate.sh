8546a5009feed1b88b508c7993079570f8361a30
Bryce Buchanan <bryce.buchanan@elastic.co>
2025-09-02 12:29:22 -0700

    generating resource and semantic attributes again

--- Scripts/semantic-convention/generate.sh
+++ Scripts/semantic-convention/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -e -x
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="${SCRIPT_DIR}/../../"
@@ -7,11 +7,11 @@ ROOT_DIR="${SCRIPT_DIR}/../../"
 # freeze the spec & generator tools versions to make SemanticAttributes generation reproducible
 
 # repository: https://github.com/open-telemetry/semantic-conventions
-SEMCONV_VERSION=1.21.0
+SEMCONV_VERSION=1.37.0
 SPEC_VERSION=v$SEMCONV_VERSION
 
 # repository: https://github.com/open-telemetry/build-tools
-GENERATOR_VERSION=0.21.0
+GENERATOR_VERSION=0.17.1
 
 cd ${SCRIPT_DIR}
 
@@ -25,28 +25,35 @@ git fetch origin "$SPEC_VERSION"
 git reset --hard FETCH_HEAD
 cd ${SCRIPT_DIR}
 
-docker run --rm \
-  -v ${SCRIPT_DIR}/semantic-conventions/model:/source \
-  -v ${SCRIPT_DIR}/templates:/templates \
-  -v ${ROOT_DIR}/Sources/OpenTelemetryApi/Trace/:/output \
-  otel/semconvgen:$GENERATOR_VERSION \
-  --only span,event,attribute_group,scope \
-  -f /source code \
-  --template /templates/SemanticAttributes.swift.j2 \
-  --output /output/SemanticAttributes.swift \
-  -Dsemconv=trace \
-  -Denum=SemanticAttributes
+rm ${ROOT_DIR}/Sources/OpenTelemetryApi/Trace/SemanticAttributes.swift || true
 
 docker run --rm \
   -v ${SCRIPT_DIR}/semantic-conventions/model:/source \
   -v ${SCRIPT_DIR}/templates:/templates \
-  -v ${ROOT_DIR}/Sources/OpenTelemetrySdk/Resources/:/output \
-  otel/semconvgen:$GENERATOR_VERSION \
-  --only resource \
-  -f /source code \
-  --template /templates/SemanticAttributes.swift.j2 \
-  --output /output/ResourceAttributes.swift \
-  -Dsemconv=resource \
+  -v ${ROOT_DIR}/Sources/OpenTelemetryApi/Trace:/output \
+  otel/weaver:v$GENERATOR_VERSION \
+  registry \
+  generate \
+  --registry=/source \
+  --templates=/templates \
+  ./ \
+  /output \
+  -Denum=SemanticAttributes
+
+
+rm ${ROOT_DIR}/Sources/OpenTelemetrySdk/Resources/ResourceAttributes.swift || true
+
+docker run --rm \
+  -v ${SCRIPT_DIR}/semantic-conventions/model:/source \
+  -v ${SCRIPT_DIR}/templates:/templates \
+  -v ${ROOT_DIR}/Sources/OpenTelemetrySdk/Resources:/output \
+  otel/weaver \
+  registry \
+  generate \
+  --registry=/source \
+  --templates=/templates \
+  ./ \
+  /output \
   -Denum=ResourceAttributes
 
 cd "$ROOT_DIR"
