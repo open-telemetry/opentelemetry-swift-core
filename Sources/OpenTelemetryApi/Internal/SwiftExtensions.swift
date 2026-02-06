1c47ce57f1314fc0f64a90cb1d08ea7babcb46f2
Billy Zhou <billyzh@amazon.com>
2026-02-06 14:36:58 -0800

    fix: Undo the breaking toNanoseconds change in SwiftExtensions by @bklimt

--- Sources/OpenTelemetryApi/Internal/SwiftExtensions.swift
+++ Sources/OpenTelemetryApi/Internal/SwiftExtensions.swift
@@ -6,22 +6,22 @@
 import Foundation
 
 public extension TimeInterval {
-  /// `TimeInterval` represented in milliseconds (clamped to [`Int64.min`, `Int64.max`]).
-  var toMilliseconds: Int64 {
+  /// `TimeInterval` represented in milliseconds (clamped to [`0`, `UInt64.max`]).
+  var toMilliseconds: UInt64 {
     let milliseconds = self * 1_000
-    return Int64(withReportingOverflow: milliseconds) ?? (self < 0 ? .min : .max)
+    return UInt64(withReportingOverflow: milliseconds) ?? (self < 0 ? .min : .max)
   }
 
-  /// `TimeInterval` represented in microseconds (clamped to [`Int64.min`, `Int64.max`]).
-  var toMicroseconds: Int64 {
+  /// `TimeInterval` represented in microseconds (clamped to [`0`, `UInt64.max`]).
+  var toMicroseconds: UInt64 {
     let microseconds = self * 1_000_000
-    return Int64(withReportingOverflow: microseconds) ?? (self < 0 ? .min : .max)
+    return UInt64(withReportingOverflow: microseconds) ?? (self < 0 ? .min : .max)
   }
 
-  /// `TimeInterval` represented in nanoseconds (clamped to [`Int64.min`, `Int64.max`]).
-  var toNanoseconds: Int64 {
+  /// `TimeInterval` represented in nanoseconds (clamped to [`0`, `UInt64.max`]).
+  var toNanoseconds: UInt64 {
     let nanoseconds = self * 1_000_000_000
-    return Int64(withReportingOverflow: nanoseconds) ?? (self < 0 ? .min : .max)
+    return UInt64(withReportingOverflow: nanoseconds) ?? (self < 0 ? .min : .max)
   }
 
   static func fromMilliseconds(_ millis: Int64) -> TimeInterval {
@@ -33,6 +33,36 @@ public extension TimeInterval {
   }
 
   static func fromNanoseconds(_ nanos: Int64) -> TimeInterval {
+    return Double(nanos) / 1_000_000_000
+  }
+  
+  /// `TimeInterval` represented in milliseconds (clamped to [`Int64.min`, `Int64.max`]).
+  var toSignedMilliseconds: Int64 {
+    let milliseconds = self * 1_000
+    return Int64(withReportingOverflow: milliseconds) ?? (self < 0 ? .min : .max)
+  }
+
+  /// `TimeInterval` represented in microseconds (clamped to [`Int64.min`, `Int64.max`]).
+  var toSignedMicroseconds: Int64 {
+    let microseconds = self * 1_000_000
+    return Int64(withReportingOverflow: microseconds) ?? (self < 0 ? .min : .max)
+  }
+
+  /// `TimeInterval` represented in nanoseconds (clamped to [`Int64.min`, `Int64.max`]).
+  var toSignedNanoseconds: Int64 {
+    let nanoseconds = self * 1_000_000_000
+    return Int64(withReportingOverflow: nanoseconds) ?? (self < 0 ? .min : .max)
+  }
+
+  static func fromSignedMilliseconds(_ millis: Int64) -> TimeInterval {
+    return Double(millis) / 1_000
+  }
+
+  static func fromSignedMicroseconds(_ micros: Int64) -> TimeInterval {
+    return Double(micros) / 1_000_000
+  }
+
+  static func fromSignedNanoseconds(_ nanos: Int64) -> TimeInterval {
     return Double(nanos) / 1_000_000_000
   }
 }
