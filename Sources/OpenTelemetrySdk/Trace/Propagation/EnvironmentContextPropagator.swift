8f57c376ba03d3bffb8e0ae89b389fb37215b4b2
Robert Pająk <pellared@hotmail.com>
2026-03-30 16:18:42 +0000

    revive EnvironmentContextPropagator as deprecated

--- Sources/OpenTelemetrySdk/Trace/Propagation/EnvironmentContextPropagator.swift
+++ Sources/OpenTelemetrySdk/Trace/Propagation/EnvironmentContextPropagator.swift
@@ -6,6 +6,32 @@
 import Foundation
 import OpenTelemetryApi
 
+
+@available(*, deprecated, message: "Use EnvironmentMappingSetter or EnvironmentMappingGetter instead.")
+public struct EnvironmentContextPropagator: TextMapPropagator {
+  static let traceParent = "TRACEPARENT"
+  static let traceState = "TRACESTATE"
+  let w3cPropagator = W3CTraceContextPropagator()
+
+  public let fields: Set<String> = [traceState, traceParent]
+
+  public init() {}
+
+  public func inject(spanContext: SpanContext, carrier: inout [String: String], setter: some Setter) {
+    var auxCarrier = [String: String]()
+    w3cPropagator.inject(spanContext: spanContext, carrier: &auxCarrier, setter: setter)
+    carrier[EnvironmentContextPropagator.traceParent] = auxCarrier["traceparent"]
+    carrier[EnvironmentContextPropagator.traceState] = auxCarrier["tracestate"]
+  }
+
+  public func extract(carrier: [String: String], getter: some Getter) -> SpanContext? {
+    var auxCarrier = [String: String]()
+    auxCarrier["traceparent"] = carrier[EnvironmentContextPropagator.traceParent]
+    auxCarrier["tracestate"] = carrier[EnvironmentContextPropagator.traceState]
+    return w3cPropagator.extract(carrier: auxCarrier, getter: getter)
+  }
+}
+
 /**
  * EnvironmentMappingSetter adapts a `Setter` to normalize propagation keys to environment variable format.
  *
